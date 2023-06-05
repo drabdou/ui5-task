0e3514a99be934c99027181575a456ae50819757
Abdeljabbar Driss <drissabdeljabbar@gmail.com>
2023-06-05 16:10:41 +0200

    Optimierung- Refresh List

--- webapp/modules/Lieferant/LieferantList.controller.js
+++ webapp/modules/Lieferant/LieferantList.controller.js
@@ -14,8 +14,7 @@ sap.ui.define(["de/bauerberatung/ui5-task/controller/BaseController", "sap/ui/mo
     },
 
     onSearch: function (oEvent) {
-      // #TodoFilter
-      
+      // #TodoFilter      
       var oList = this.getView().byId("supplierList");
       var oSearchField = this.getView().byId("searchField").getValue();
       //sQuery is used as the search value when filtering the list, initialized as an empty string
@@ -27,39 +26,20 @@ sap.ui.define(["de/bauerberatung/ui5-task/controller/BaseController", "sap/ui/mo
         sQuery = oSearchField;
       //get the binding of the list items
       var oBinding = oList.getBinding("items");
-      //Creating Filter to filter list by CompanyName (Lieferantenname)
+      //Creating the filter by CompanyName (Lieferantenname)
       var oCompanyNameFilter = new sap.ui.model.Filter("CompanyName", sap.ui.model.FilterOperator.Contains, sQuery);
-      //Creating Filter to filter list by SupplierId (Nummer)
+      //Creating Filter by SupplierId (Nummer)
       var oSupplierIdFilter = new sap.ui.model.Filter("SupplierID", sap.ui.model.FilterOperator.Contains, sQuery);
       //Combining the two filters and add the search result to aFilter
       aFilter.push(new sap.ui.model.Filter([oCompanyNameFilter, oSupplierIdFilter], false));
       //Change the list binding items with result from aFilter
       oBinding.filter(aFilter);
-
-
-
-      //Here in the code below i was testing the filtering option with another way since the standard Filters properties in XML
-      //were raising an issue with the data loaded in the list and when clicking the search button
-      //with the code above this would not be working properly----->Code below was making it work just fine ;))
-
-      /*   
-       if (oSearchField != "") 
-         sQuery = oSearchField;
-       else if (oEvent == undefined || oEvent.mParameters.newValue == undefined) {
-          sQuery = ""
-          this.getView().byId("searchField").setPlaceholder("Bitte Nummer oder Liferantenname hier einfügen");
-        }
-          else
-           sQuery = oEvent.mParameters.newValue
-         // alert(sQuery);
-      // Referenz auf die Liste
-      // var oList = this.getView().byId("supplierList");  
-       */
-
     },
 
     onRefreshSupplier: function () {
-      sap.m.MessageToast.show("Dummy - Refresh");
+     // sap.m.MessageToast.show("Dummy - Refresh");
+      this.getView().byId("searchField").setValue("");
+      this.onSearch();
     },
 
     onAddSupplier: function () {
